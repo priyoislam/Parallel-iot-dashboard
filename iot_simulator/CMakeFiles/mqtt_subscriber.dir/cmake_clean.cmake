6d16447eecc588aa44accd32383f25b4affe0831
priyoislam <priyoislam@gmail.com>
2025-06-09 23:17:29 +0300

    xsd

---
+++ iot_simulator/CMakeFiles/mqtt_subscriber.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mqtt_subscriber.dir/link.d"
+  "CMakeFiles/mqtt_subscriber.dir/main.cpp.o"
+  "CMakeFiles/mqtt_subscriber.dir/main.cpp.o.d"
+  "mqtt_subscriber"
+  "mqtt_subscriber.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mqtt_subscriber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ iot_simulator/CMakeFiles/mqtt_subscriber.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mqtt_subscriber.
+# This may be replaced when dependencies are built.
